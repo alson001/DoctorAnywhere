23ecc3b82603209d3807ca4bdfe5129c9d204477
alson001 <alsonang01@gmail.com>
2023-03-25 13:14:16 +0800

    Create Spring Web Framework

---
+++ src/main/java/com/example/DoctorAnywhere/DoctorAnywhereApplication.java
@@ -0,0 +1,13 @@
+package com.example.DoctorAnywhere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorAnywhereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorAnywhereApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DoctorAnywhere/DoctorAnywhereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DoctorAnywhere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorAnywhereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
