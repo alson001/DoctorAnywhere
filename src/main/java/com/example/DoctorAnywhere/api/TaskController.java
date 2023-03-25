792c230b73b52d24d58dd9e62854db8f935f2297
alson001 <alsonang01@gmail.com>
2023-03-25 15:28:01 +0800

    Add validation

--- src/main/java/com/example/DoctorAnywhere/api/TaskController.java
+++ src/main/java/com/example/DoctorAnywhere/api/TaskController.java
@@ -4,6 +4,7 @@ import com.example.DoctorAnywhere.model.Task;
 import com.example.DoctorAnywhere.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -27,7 +28,7 @@ public class TaskController {
     }
 
     @PostMapping
-    public void addTask(@NonNull @RequestBody Task task) {
+    public void addTask(@Validated @NonNull @RequestBody Task task) {
         taskService.addTask(task);
     }
 
